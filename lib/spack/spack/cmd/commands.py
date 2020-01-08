5302ea9f4d18c8c5148ffa65e88a6de337959571
Adam J. Stewart <ajstewart426@gmail.com>
2020-01-08 13:01:41 -0600

    spack completion -> spack commands --format=bash

--- lib/spack/spack/cmd/commands.py
+++ lib/spack/spack/cmd/commands.py
@@ -70,6 +70,50 @@ class SubcommandWriter(ArgparseWriter):
         self.out.write('\n')
 
 
+class StackEntry():
+    def __init__(self):
+        self.optionals = []
+        self.subcommands = []
+
+
+class SpackBashCompletionWriter(ArgparseWriter):
+    def __init__(self, out=sys.stdout):
+        super(SpackBashCompletionWriter, self).__init__(out)
+        self.stack = []
+
+    def begin_command(self, name):
+        self.stack.append(StackEntry())
+
+    def optional(self, opts, help):
+        cmd = self.stack[-1]
+
+        # get rid of choice lists like {true false}
+        opts = re.sub(r'\{[^}]*\}', '', opts).strip()
+
+        # get rid of metavars (which do not start with `-`)
+        opts = [opt for opt in re.split(r',?\s+', opts) if opt.startswith('-')]
+        cmd.optionals.extend(opts)
+
+    def begin_subcommands(self, subcommands):
+        cmd = self.stack[-1]
+        for full_cmd in subcommands:
+            parts = re.split(r'\s+', full_cmd.prog)
+            cmd.subcommands.append(parts[-1])
+
+    def end_command(self, name):
+        cmd = self.stack.pop()
+        print("function _%s {" % name.replace(" ", "_"))
+        print("    if $list_options")
+        print("    then")
+        print('        compgen -W "%s" -- "$cur"' % " ".join(cmd.optionals))
+        if cmd.subcommands:
+            print("    else")
+            print('        compgen -W "%s" -- "$cur"' % " ".join(
+                cmd.subcommands))
+        print("    fi")
+        print("}\n")
+
+
 @formatter
 def subcommands(args, out):
     parser = spack.main.make_argument_parser()
@@ -167,3 +211,12 @@ def commands(parser, args):
     else:
         prepend_header(args, sys.stdout)
         formatter(args, sys.stdout)
+
+
+@formatter
+def bash(parser, args):
+    parser = spack.main.make_argument_parser()
+    spack.main.add_all_commands(parser)
+
+    writer = SpackBashCompletionWriter()
+    writer.write(parser)
