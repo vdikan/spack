77adbdd3f35a09e13559e75884ccb1f08103849c
Vladimir Dikan <vdikan@vivaldi.net>
2020-01-14 15:24:45 +0100

    edited xmlf90 package; bumped version up to 1.5.4

--- var/spack/repos/builtin/packages/xmlf90/package.py
+++ var/spack/repos/builtin/packages/xmlf90/package.py
@@ -11,25 +11,37 @@ class Xmlf90(AutotoolsPackage):
     """xmlf90 is a suite of libraries to handle XML in Fortran."""
 
     homepage = "https://launchpad.net/xmlf90"
-    url      = "https://launchpad.net/xmlf90/trunk/1.5/+download/xmlf90-1.5.2.tgz"
+    url      = "https://launchpad.net/xmlf90/trunk/1.5/+download/xmlf90-1.5.4.tar.gz"
+    git      = "https://gitlab.com/siesta-project/libraries/xmlf90.git"
 
-    version('1.5.2', sha256='666694db793828d1d1e9aea665f75c75ee21772693465a88b43e6370862abfa6')
+    maintainers = ['vdikan']
 
-    depends_on('autoconf@2.69:', type='build')
-    depends_on('automake@1.14:', type='build')
-    depends_on('libtool@2.4.2:', type='build')
-    depends_on('m4',             type='build')
+    # version('master',  branch='master')
 
+    version('1.5.4', sha256='a0b1324ff224d5b5ad1127a6ad4f90979f6b127f1a517f98253eea377237bbe4')
+    version('1.5.3', sha256='a5378a5d9df4b617f51382092999eb0f20fa1a90ab49afbccfd80aa51650d27c')
+    version('1.5.2', sha256='666694db793828d1d1e9aea665f75c75ee21772693465a88b43e6370862abfa6',
+            url="https://launchpad.net/xmlf90/trunk/1.5/+download/xmlf90-1.5.2.tgz")
+
+    depends_on('autoconf', type='build', when='@1.5.2')
+    depends_on('automake', type='build', when='@1.5.2')
+    depends_on('libtool',  type='build', when='@1.5.2')
+    depends_on('m4',       type='build', when='@1.5.2')
+
+    sanity_check_is_dir  = ['lib', 'share', 'include']
+    sanity_check_is_file = [join_path('share', 'org.siesta-project',
+                                      'xmlf90.mk')]
+
+    @when('@1.5.2')
     def autoreconf(self, spec, prefix):
-        sh = which('sh')
-        sh('autogen.sh')
+        autoreconf('--install', '--verbose', '--force')
 
     def configure_args(self):
         if self.spec.satisfies('%gcc'):
             return ['FCFLAGS=-ffree-line-length-none']
         return []
 
-    @run_after('install')
-    def fix_mk(self):
-        install(join_path(self.prefix, 'share', 'org.siesta-project',
-                          'xmlf90.mk'), prefix)
+    # @run_after('install')
+    # def fix_mk(self):
+    #     install(join_path(self.prefix, 'share', 'org.siesta-project',
+    #                       'xmlf90.mk'), prefix)
